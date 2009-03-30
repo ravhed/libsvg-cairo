0bd2691351d1c9a1d14a663fede52cf53cfcf3dd
Philip de Nier <philipn@users.sourceforge.net>
2009-03-30 12:18:29 +0000

    using libsvg utility function to convert from elliptical arc endpoint parameterization to center parameterization;

--- src/svg_cairo.c
+++ src/svg_cairo.c
@@ -2060,32 +2060,31 @@ _svg_path_arc_segment (cairo_t *cr,
 		       double th0, double th1,
 		       double rx, double ry, double x_axis_rotation)
 {
-    double sin_th, cos_th;
-    double a00, a01, a10, a11;
     double x1, y1, x2, y2, x3, y3;
     double t;
     double th_half;
+    double f, sinf, cosf;
 
-    sin_th = sin (x_axis_rotation * (M_PI / 180.0));
-    cos_th = cos (x_axis_rotation * (M_PI / 180.0)); 
-    /* inverse transform compared with rsvg_path_arc */
-    a00 = cos_th * rx;
-    a01 = -sin_th * ry;
-    a10 = sin_th * rx;
-    a11 = cos_th * ry;
+    f = x_axis_rotation * M_PI / 180.0;
+    sinf = sin(f);
+    cosf = cos(f);
 
     th_half = 0.5 * (th1 - th0);
     t = (8.0 / 3.0) * sin (th_half * 0.5) * sin (th_half * 0.5) / sin (th_half);
-    x1 = xc + cos (th0) - t * sin (th0);
-    y1 = yc + sin (th0) + t * cos (th0);
-    x3 = xc + cos (th1);
-    y3 = yc + sin (th1);
-    x2 = x3 + t * sin (th1);
-    y2 = y3 - t * cos (th1);
+    x1 = rx * (cos (th0) - t * sin (th0));
+    y1 = ry * (sin (th0) + t * cos (th0));
+    x3 = rx * cos (th1);
+    y3 = ry * sin (th1);
+    x2 = x3 + rx * (t * sin (th1));
+    y2 = y3 - ry * (t * cos (th1));
 
-    cairo_curve_to (cr, a00 * x1 + a01 * y1, a10 * x1 + a11 * y1,
-		a00 * x2 + a01 * y2, a10 * x2 + a11 * y2,
-		a00 * x3 + a01 * y3, a10 * x3 + a11 * y3);
+    cairo_curve_to (cr,
+		    xc + cosf * x1 - sinf * y1,
+		    yc + sinf * x1 + cosf * y1,
+		    xc + cosf * x2 - sinf * y2,
+		    yc + sinf * x2 + cosf * y2,
+		    xc + cosf * x3 - sinf * y3,
+		    yc + sinf * x3 + cosf * y3);
 }
 
 /**
@@ -2111,69 +2110,28 @@ _svg_cairo_arc_to (void		*closure,
 	      double		y)
 {
     svg_cairo_t *svg_cairo = closure;
-    double sin_th, cos_th;
-    double a00, a01, a10, a11;
-    double x0, y0, x1, y1, xc, yc;
-    double d, sfactor, sfactor_sq;
-    double th0, th1, th_arc;
+    svg_path_arc_to_t e_arc;
+    svg_center_point_arc_t c_arc;
+    double cur_x, cur_y;
+    int result;
     int i, n_segs;
-    double dx, dy, dx1, dy1, Pr1, Pr2, Px, Py, check;
-    double curx, cury;
 
-    rx = fabs (rx);
-    ry = fabs (ry);
+    cairo_get_current_point (svg_cairo->cr, &cur_x, &cur_y);
 
-    cairo_get_current_point (svg_cairo->cr, &curx, &cury);
+    e_arc.rx = rx;
+    e_arc.ry = ry;
+    e_arc.x_axis_rotation = x_axis_rotation;
+    e_arc.large_arc_flag = large_arc_flag;
+    e_arc.sweep_flag = sweep_flag;
+    e_arc.x = x;
+    e_arc.y = y;
 
-    sin_th = sin (x_axis_rotation * (M_PI / 180.0));
-    cos_th = cos (x_axis_rotation * (M_PI / 180.0));
+    result = svg_center_point_param_arc (cur_x, cur_y, &e_arc, &c_arc);
+    if (result != 0)
+	return SVG_STATUS_SUCCESS; /* XXX: return error instead? */
 
-    dx = (curx - x) / 2.0;
-    dy = (cury - y) / 2.0;
-    dx1 =  cos_th * dx + sin_th * dy;
-    dy1 = -sin_th * dx + cos_th * dy;
-    Pr1 = rx * rx;
-    Pr2 = ry * ry;
-    Px = dx1 * dx1;
-    Py = dy1 * dy1;
-    /* Spec : check if radii are large enough */
-    check = Px / Pr1 + Py / Pr2;
-    if(check > 1)
-    {
-        rx = rx * sqrt(check);
-        ry = ry * sqrt(check);
-    }
-
-    a00 = cos_th / rx;
-    a01 = sin_th / rx;
-    a10 = -sin_th / ry;
-    a11 = cos_th / ry;
-    x0 = a00 * curx + a01 * cury;
-    y0 = a10 * curx + a11 * cury;
-    x1 = a00 * x + a01 * y;
-    y1 = a10 * x + a11 * y;
-    /* (x0, y0) is current point in transformed coordinate space.
-       (x1, y1) is new point in transformed coordinate space.
-       
-       The arc fits a unit-radius circle in this space.
-    */
-    d = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
-    sfactor_sq = 1.0 / d - 0.25;
-    if (sfactor_sq < 0) sfactor_sq = 0;
-    sfactor = sqrt (sfactor_sq);
-    if (sweep_flag == large_arc_flag) sfactor = -sfactor;
-    xc = 0.5 * (x0 + x1) - sfactor * (y1 - y0);
-    yc = 0.5 * (y0 + y1) + sfactor * (x1 - x0);
-    /* (xc, yc) is center of the circle. */
-    
-    th0 = atan2 (y0 - yc, x0 - xc);
-    th1 = atan2 (y1 - yc, x1 - xc);
-    
-    th_arc = th1 - th0;
-    if (th_arc < 0 && sweep_flag)
-	th_arc += 2 * M_PI;
-    else if (th_arc > 0 && !sweep_flag)
-	th_arc -= 2 * M_PI;
+    c_arc.start_angle *= M_PI / 180;
+    c_arc.delta_angle *= M_PI / 180;
 
     /* XXX: I still need to evaluate the math performed in this
        function. The critical behavior desired is that the arc must be
@@ -2181,13 +2139,15 @@ _svg_cairo_arc_to (void		*closure,
        user should be able to specify as well). I don't yet know the
        bounds of the error from the following computation of
        n_segs. Plus the "+ 0.001" looks just plain fishy. -cworth */
-    n_segs = (int)ceil (fabs (th_arc / (M_PI * 0.5 + 0.001)));
+    n_segs = (int)ceil (fabs (c_arc.delta_angle / (M_PI * 0.5 + 0.001)));
     
     for (i = 0; i < n_segs; i++) {
-	_svg_path_arc_segment (svg_cairo->cr, xc, yc,
-					th0 + i * th_arc / n_segs,
-					th0 + (i + 1) * th_arc / n_segs,
-					rx, ry, x_axis_rotation);
+	_svg_path_arc_segment (svg_cairo->cr,
+			       c_arc.cx, c_arc.cy,
+			       c_arc.start_angle + i * c_arc.delta_angle / n_segs,
+			       c_arc.start_angle + (i + 1) * c_arc.delta_angle / n_segs,
+			       c_arc.rx, c_arc.ry,
+			       x_axis_rotation);
     }
 
     return _cairo_status_to_svg_status (cairo_status (svg_cairo->cr));
